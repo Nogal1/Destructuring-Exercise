3e4e7487cd4b44e5d1688b67f99caa0d79fad5c9
Nogal <logan.mccl0406@gmail.com>
2024-07-12 17:34:54 -0700

    just kidding I didn't complete task

--- script.js
+++ script.js
@@ -71,3 +71,45 @@ let numbers = [10, 20, 30];
 [numbers[1], numbers[2]] = [numbers[2], numbers[1]]
 
 console.log(numbers) // [10, 30, 20]
+
+
+//ES2015 Refactoring
+
+// ES5 Assigning Variables to Object Properties
+
+// var obj = {
+//     numbers: {
+//         a: 1,
+//         b: 2
+//     }
+// };
+
+// var a = obj.numbers.a;
+// var b = obj.numbers.b;
+
+/* Write an ES2015 Version */
+
+const obj = {
+    numbers: {
+        a: 1,
+        b: 2,
+    }
+};
+
+const { a, b } = obj.numbers
+
+
+// ES5 Array Swap
+
+// var arr = [1, 2];
+// var temp = arr[0];
+// arr[0] = arr[1];
+// arr[1] = temp;
+
+// /* Write an ES2015 Version */
+let arr = [1, 2];
+[arr[0], arr[1]] = [arr[1], arr[0]];
+console.log(arr);
+
+const raceResults = ([first, second, third, ...rest]) => ({ first, second, third, rest });
+raceResults(['Tom', 'Margaret', 'Allison', 'David', 'Pierre']);
